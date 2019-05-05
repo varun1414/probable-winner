5096d6baacfaa40f42a864593ed704ddc0762294
varun1414 <41757833+varun1414@users.noreply.github.com>
2019-05-05 21:20:53 +0530

    Add files via upload

---
+++ prob.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		double a,b,c,d;
+		cin>>a>>b>>c>>d;
+		cout<<endl<<a/(a+b)<<endl;
+	}
+	return 0;
+}
